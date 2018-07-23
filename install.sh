e93d025c564f8be021a30bf1f570f6091328e0b1
Nathaniel Cook <nvcook42@gmail.com>
2018-07-23 14:45:32 -0600

    add more logic to the install script

--- install.sh
+++ install.sh
@@ -8,23 +8,75 @@
 #
 #   apt-get install docker.io kubelet kubeadm kubernetes-cni
 
-DOMAIN=myorg.internal
+set -e
+
+DOMAIN=${DOMAIN-slate.internal}
+#IP=$(hostname --ip-address | awk '{print $1}')
+
+bind_dirs='/etc/kubernetes/pki /etc/kubernetes/scheduler.conf /etc/ssl/certs /var/lib/etcd'
+for dir in $bind_dirs
+do
+    sudo umount $dir $dir || true
+done
+
+sudo kubeadm reset
+
+sudo mkdir -p /etc/systemd/system/kubelet.service.d/
+sudo cp ./override.conf /etc/systemd/system/kubelet.service.d/
+
+sudo mkdir -p /etc/kubernetes
+sudo cp ./kubelet.system.config /etc/kubernetes/
+
+sudo systemctl daemon-reload
+
+# Bind paths as workaround to https://github.com/moby/moby/issues/37032
+#for dir in $bind_dirs
+#do
+#    sudo touch $dir
+#    sudo mount --bind $dir $dir
+#done
 
 # Run kubeadm to setup kubelet systemd service.
 # Check for any warnings and attempt to resolve.
 sudo kubeadm init \
+    --kubernetes-version 1.10.3 \
+    --apiserver-advertise-address 127.0.0.1 \
+    --apiserver-cert-extra-sans 127.0.0.1 \
     --service-dns-domain $DOMAIN \
-    --pod-network-cidr 192.168.100.0/24
+    --service-cidr 10.42.0.0/16 \
+    --pod-network-cidr 192.168.100.0/24 \
+    --ignore-preflight-errors=Swap
+    #--node-name localhost \
 
 # The flag --service-dns-domain doesn't work
 # Stop kublet and update manually
 # Edit: /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-# Replace cluster.local with $DOMAIN
+sudo systemctl stop kubelet
+sudo sed -i "s/cluster\.local/$DOMAIN/" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sudo sed -i "s/10\.96\.0\.10/10.42.0.10/" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+#sudo sed -i "s/KUBELET_DNS_ARGS=/KUBELET_DNS_ARGS=--hostname-override 127.0.0.1 /" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+#if [ -z $IP ]
+#then
+#    sudo find /etc/kubernetes -type f | xargs sudo sed -i "s/$IP/127.0.0.1/"
+#fi
+sudo systemctl daemon-reload
+sudo systemctl start kubelet
 
 
 # Setup kubectl
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+mkdir -p $HOME/.kube
+sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+mkdir -p /root
+sudo cp /etc/kubernetes/admin.conf /root/.kube/config
+
+# Reapply manifests
+
+#for m in $(sudo ls /etc/kubernetes/manifests)
+#do
+#    sudo kubectl apply -f /etc/kubernetes/manifests/$m
+#done
 
 # Add Kube-router
 kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
